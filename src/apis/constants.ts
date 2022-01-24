4a410e6d00543b76c9a8ea99d50c7d0fa84e30ff
정훈박 <fireking5997@gmail.com>
2022-01-24 19:15:19 +0900

    Fix:오타수정

--- src/apis/constants.ts
+++ src/apis/constants.ts
@@ -1,5 +1,5 @@
 const key = "78c4651e6f70ef92cb879b749825122d";
-export const BASIC_URL = "http://api.themoviedb.org/3";
+export const BASIC_URL = "https://api.themoviedb.org/3";
 export const MOVIE_URL = `${BASIC_URL}/movie`;
 export const TV_URL = `${BASIC_URL}/tv`;
 export const API_PARAM = `?api_key=${key}`;
