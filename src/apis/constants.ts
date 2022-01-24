00b043fac14784781c4d47f45f7379d30fed318b
정훈박 <fireking5997@gmail.com>
2022-01-24 19:03:57 +0900

    Fix:key 추가

--- src/apis/constants.ts
+++ src/apis/constants.ts
@@ -1,4 +1,4 @@
-const key = process.env.REACT_APP_KEY;
+const key = "78c4651e6f70ef92cb879b749825122d";
 export const BASIC_URL = "https://api.themoviedb.org/3";
 export const MOVIE_URL = `${BASIC_URL}/movie`;
 export const TV_URL = `${BASIC_URL}/tv`;
